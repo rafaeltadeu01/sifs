1fe5693ef15bfebed9b0f4244fa2d970cf1d7519
Rafael <rafaeltadeu01@hotmail.com>
2021-09-06 12:05:14 -0300

    Atualização de Novos Sistemas

---
+++ docker-compose/minio/README.md
@@ -0,0 +1,2 @@
+## Documentação
+https://docs.min.io/docs/deploy-minio-on-docker-compose.html
---
+++ scripts/scripst-em-avaliação/install-ocomon.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker run -it --name ocomon -p 8000:80 flaviorib/ocomon_demo-3.3:3.3 /bin/ocomon
